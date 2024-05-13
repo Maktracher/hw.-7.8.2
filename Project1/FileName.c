ed85e3efdf61183daf4b0381b695e3501e771ef0
Богдан Ільніцький <bdancik8@gmail.com>
2024-05-13 15:22:50 +0300

    Добавьте файлы проекта.

---
+++ Project1/FileName.c
@@ -0,0 +1,16 @@
+﻿#include <stdlib.h>
+#include <stdio.h>
+<<<<<< < Updated upstream
+#include <time.h> // Pridané na použitie funkcie time().
+#pragma warning (disable:4996) // Pridané pre kompilátor VS
+
+	char svg_draw_permutation(int* perm, int n) {
+	if (perm == NULL || n <= 0) { // Overenie
+		printf("Error: Nesprávne vstupné údaje");
+		return -1;
+	}
+
+}
+int main() {
+
+}
