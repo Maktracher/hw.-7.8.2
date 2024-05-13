197b5d14b47e113830829a47a2170838df0ada61
Богдан Ільніцький <bdancik8@gmail.com>
2024-05-13 15:35:08 +0300

    Kreslenie permutačných čiar

--- Project1/FileName.c
+++ Project1/FileName.c
@@ -18,8 +18,18 @@
 	fprintf(svg_file, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"); // Hlavička SVG
 	fprintf(svg_file, "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"%d\" height=\"%d\">\n", width, height); // Otvorte značku SVG
 
+	for (int i = 0; i < n; ++i) { // Kreslenie permutačných čiar
+		fprintf(svg_file, "<line x1='%d' y1='%d' x2='%d' y2='%d' stroke='green' />\n",
+			50 * i + 25, 25, 50 * perm[i] + 25, 50 * n - 25);
+	}
 
+	fprintf(svg_file, "</svg>"); // Zatvorte značku SVG
+	fclose(svg_file); // Zatvorte súbor
+
+	printf("Obraz bol úspešne vytvorený ");
+	return 1;
 }
+
 int main() {
 
 }
