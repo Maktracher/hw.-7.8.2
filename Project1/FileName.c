e7d7af1902297980c0d70906ce928df334b44da6
Богдан Ільніцький <bdancik8@gmail.com>
2024-06-21 13:59:25 +0300

    index a arrow

--- Project1/FileName.c
+++ Project1/FileName.c
@@ -1,9 +1,10 @@
 ﻿#include <stdlib.h>
 #include <stdio.h>
-#include <time.h> // Pridané na použitie funkcie time().
-#pragma warning (disable:4996) // Pridané pre kompilátor VS
+#include <time.h>
+#include <math.h> // Pridané na použitie matematických funkcií
 
-#define PERMUTATION_SIZE 10  // Konštanta pre veľkosť permutácie
+#define PERMUTATION_SIZE 10 // Konštanta pre veľkosť permutácie
+#define M_PI 3.14159265358979323846
 
 // Funkcia na vizualizáciu permutácie vo formáte SVG
 char svg_draw_permutation(int* perm, int n) {
@@ -20,27 +21,46 @@ char svg_draw_permutation(int* perm, int n) {
     fprintf(svg_file, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"); // Hlavička SVG
     fprintf(svg_file, "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"%zu\" height=\"%zu\">\n", width, height); // Otvorte značku SVG
 
-    // Kreslenie indexov nad a pod obrázkom
+    // Kreslíme indexy nad a pod obrázkom
     for (int i = 0; i < n; ++i) {
         fprintf(svg_file, "<text x='%d' y='15' font-size='12' text-anchor='middle'>%d</text>\n", 50 * i + 25, i);
         fprintf(svg_file, "<text x='%d' y='%zu' font-size='12' text-anchor='middle'>%d</text>\n", 50 * i + 25, height - 10, perm[i]);
     }
 
-    for (int i = 0; i < n; ++i) { // Kreslenie permutačných čiar
-        fprintf(svg_file, "<line x1='%d' y1='%d' x2='%d' y2='%d' stroke='green' />\n",
-            50 * i + 25, 25, 50 * perm[i] + 25, 50 * n - 25);
-    }
+    // Kreslíme vektory so "šípkami" na koncoch
+    for (int i = 0; i < n; ++i) {
+        int startX = 50 * i + 25;
+        int startY = height - 25;
+        int endX = 50 * perm[i] + 25;
+        int endY = 25;
 
-    fprintf(svg_file, "<defs>\n<marker id='arrow' markerWidth='10' markerHeight='10' refX='0' refY='3' orient='auto' markerUnits='strokeWidth'>\n");
-    fprintf(svg_file, "<path d='M0,0 L0,6 L9,3 z' fill='green' />\n</marker>\n</defs>\n");
+        // Kreslíme líniu vektora
+        fprintf(svg_file, "<line x1='%d' y1='%d' x2='%d' y2='%d' stroke='green' stroke-width='2' />\n", startX, startY, endX, endY);
+
+        // Nájdeme uhol smeru vektora
+        double angle = atan2(endY - startY, endX - startX);
+
+        // Dĺžka "šípky"
+        double arrow_length = 15.0;
+        double arrow_angle = M_PI / 6;
+
+        // Koordináty koncov "šípky"
+        double arrow_end1_x = endX - arrow_length * cos(angle - arrow_angle);
+        double arrow_end1_y = endY - arrow_length * sin(angle - arrow_angle);
+        double arrow_end2_x = endX - arrow_length * cos(angle + arrow_angle);
+        double arrow_end2_y = endY - arrow_length * sin(angle + arrow_angle);
+
+        // Kreslíme "šípku" na konci vektora
+        fprintf(svg_file, "<polygon points='%d,%d %d,%d %d,%d' style='fill:green;' />\n",
+            (int)arrow_end1_x, (int)arrow_end1_y, endX, endY, (int)arrow_end2_x, (int)arrow_end2_y);
+    }
 
     fprintf(svg_file, "</svg>");
     fclose(svg_file);
 
-    printf("Obraz bol úspešne vytvorený\n");
+    printf("SVG súbor úspešne vytvorený: permutation.svg\n");
     return 1;
 }
-
 int main() {
     srand((unsigned)time(NULL));
 
